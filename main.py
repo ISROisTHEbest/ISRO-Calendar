1ca389f5e57eb77fad5f0eeae7eb5c773f61a1c7
ISROisTHEbest <taksheelshah2009@gmail.com>
2026-01-05 18:14:46 +0530

    V1.2.0 Switched to different library (ics -> icalendar) to improve efficiency

--- main.py
+++ main.py
@@ -1,47 +1,54 @@
 import requests
-from ics import Calendar, Event
-from ics.grammar.parse import ContentLine
+from icalendar import Calendar, Event
+from datetime import datetime
 import os
 
 calendar_path = 'docs/isro_launches.ics'
 base_url = 'https://ll.thespacedevs.com/2.3.0/'
 
 if os.path.exists(calendar_path):
-    with open(calendar_path, 'r', encoding='utf-8', errors='replace') as file:
-        calendar = Calendar(file.read())
+    with open(calendar_path, 'rb') as file:
+        calendar = Calendar.from_ical(file.read())
 else:
     calendar = Calendar()
-    calendar.scale = 'Gregorian'
-    calendar.extra.append(ContentLine(name="X-WR-CALNAME", value="ISRO Launches"))
-    calendar.extra.append(ContentLine(name="X-PUBLISHED-TTL", value="PT1H"))
-    calendar.extra.append(ContentLine(name="REFRESH-INTERVAL;VALUE=DURATION", value="PT1H"))
+    calendar.calendar_name = 'ISRO Launches'
+    calendar.add('scale', 'Gregorian')
+    calendar.add('x-published-ttl', 'PT1H')
+    calendar.add('refresh-interval', value='PT1H', parameters={'value': 'DURATION'})
 
 data = requests.get(base_url+'launches/upcoming', params={'format':'json', 'lsp__id':'31,1051'}).json()
 
 for launch in data['results']:
-    for event in calendar.events:
-        if launch['id'] == event.uid:
-            calendar.events.discard(event)
-            break
-    event = Event()
-    event.uid = launch['id']
-    event.name = launch['name']
-    if launch['window_start'] != launch['window_end']:
-        event.begin = str(launch['window_start']).replace('T', ' ').replace('Z', '')
-        event.end = str(launch['window_end']).replace('T', ' ').replace('Z', '')
+    i = 0
+    for subcomponent in calendar.subcomponents:
+        if subcomponent.name == 'VEVENT':
+            if launch['id'] == str(subcomponent.get('uid')):
+                calendar.subcomponents.remove(subcomponent)
+                break
+        i += 1
     else:
-        event.begin = str(launch['window_start']).replace('T', ' ').replace('Z', '')
-        event.make_all_day()
+        i = None
+    event = Event()
+    event.add('uid', launch['id'])
+    event.add('summary', launch['name'])
+    window_start = datetime.strptime(launch['window_start'], '%Y-%m-%dT%H:%M:%S%z')
+    if launch['window_start'] != launch['window_end']:
+        event.add('dtstart', window_start)
+        event.add('dtend', datetime.strptime(launch['window_end'], '%Y-%m-%dT%H:%M:%S%z'))
+    else:
+        event.add('dtstart', window_start.date())
     try:
         desc = str(launch['mission']['description']).replace('\r', '')
-        event.description = f"[{launch['launch_service_provider']['name']}]\n{launch['rocket']['configuration']['full_name']}\n\n{desc}"
+        event.add('description', f"[{launch['launch_service_provider']['name']}]\n{launch['rocket']['configuration']['full_name']}\n\n{desc}")
     except:
-        event.description = f"[{launch['launch_service_provider']['name']}]\n{launch['rocket']['configuration']['full_name']}"
+        event.add('description', f"[{launch['launch_service_provider']['name']}]\n{launch['rocket']['configuration']['full_name']}")
     
-    event.location = launch['pad']['name']
+    event.add('location', launch['pad']['name'])
+    if i == None:
+        calendar.subcomponents.append(event)
+    else:
+        calendar.subcomponents.insert(i, event)
 
-    calendar.events.add(event)
-
-with open('docs/isro_launches.ics', 'w', encoding='utf-8') as file:
-    file.writelines(calendar.serialize_iter())
+with open('docs/isro_launches.ics', 'wb') as file:
+    file.write(calendar.to_ical())
 
